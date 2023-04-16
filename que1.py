d0dd6859daaa066db8e515e42160c1b9402e9f13
Gaurvk010 <130918913+Gaurvk010@users.noreply.github.com>
2023-04-16 21:19:34 +0530

    Add files via upload

---
+++ que1.py
@@ -0,0 +1,6 @@
+#to get the input from the users in 'integer' value format
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+average = (num1+num2+num3)/3 #this equation is used to calculate the average
+print("The average of the numbers entered=", average)
